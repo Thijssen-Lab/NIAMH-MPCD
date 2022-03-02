89dbbf04d8e73b3c10304cca69d192f2b5a761fe
Karolina <karolina@karolinas-mbp.home>
2022-03-02 17:39:24 +0000

    wavy walls with rules, non-planar wavy not tested

--- mpcd/subroutines/mpc.c
+++ mpcd/subroutines/mpc.c
@@ -513,10 +513,10 @@ void gridShift_all( double SHIFT[],int shiftBack,particleMPC *SRDparticles,bc WA
 	//Shift each particle
 	for( i=0; i<GPOP; i++ ) for( j=0; j<DIM; j++ ){
 		 SRDparticles[i].Q[j] += signedSHIFT[j]; // perform the shift
-		
+
 		 // The Gallilean shift can (sometimes) force particles outside the boundaries of the system.
 		 // 	These lines will periodically shift those particles to the other side of the domain, while
-		 //	keeping those that escaped naturally (ie not due to the shift) in place. It will also return 
+		 //	keeping those that escaped naturally (ie not due to the shift) in place. It will also return
 		 //	them to the correct place when needing to shift back for streaming.
 		 if (shiftBack == 0 && SRDparticles[i].Q[j] > XYZ[j] && SRDparticles[i].Q[j] <= XYZ[j] + signedSHIFT[j] )	SRDparticles[i].Q[j] -= XYZ[j];
 		 else if (shiftBack == 1 && SRDparticles[i].Q[j] < 0.0 && SRDparticles[i].Q[j] >= signedSHIFT[j] ) SRDparticles[i].Q[j] += XYZ[j];
