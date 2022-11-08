9ccf8491c16b65c131da48f2416df54726d23564
Tyler <t.shendru@ec.ac.uk>
2022-11-08 15:52:00 +0000

    Removed minor duplication in SRDclss.h due to large merge from TK-RemoveDupes branch

--- mpcd/headers/SRDclss.h
+++ mpcd/headers/SRDclss.h
@@ -39,8 +39,6 @@ typedef struct spec {
 	double SIGWIDTH;		//The width of the sigmoid for active dipole sigmoid (CO#20)
 	double SIGPOS;			//The position of the sigmoid for active dipole sigmoid (CO#20)
 	double MINACTRATIO;		//Minimum proportion of particles in the cell for activity to be applied
-	double DAMP;			//A damping/friction coefficient to go from wet to dry (to kill hydrodynamics) [0,1]
-	double M[MAXSPECI];	//Interaction matrix for multiphase fluids --- each species has a different interaction with all others
 } spec;
 typedef struct bc {
 /*
