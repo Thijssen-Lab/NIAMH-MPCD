51f8dbf6b58bb8b82c40b41f1594369aa9bf6d8c
zahra-valei <86073153+zahra-valei@users.noreply.github.com>
2023-02-07 12:58:58 +0000

    ctool docs

--- mpcd/subroutines/ctool.c
+++ mpcd/subroutines/ctool.c
@@ -1,3 +1,7 @@
+///
+/// @file
+///
+
 # include<stdio.h>
 # include<math.h>
 
@@ -8,20 +12,24 @@
 /* ****************************************** */
 /* ****************************************** */
 /* ****************************************** */
+///
+/// @brief		Waits for the user to press enter.
+///		
 void wait4u() {
-/*
-    Waits for the user to press enter
-*/
 	printf( "\nPress enter to continue\n" );
 	while ( 1 ) {
 		if( '\n' == getchar() )
 		break;
 	}
 }
+
+///
+/// @brief			    Prints a warning message if the value is within the precision cutoff for zero. 
+///
+/// @param test			The value that will be checked.
+/// @param zero 		The check value for zero.
+/// @param mult 		The coefficient that changes the magnitude of `zero`.
+///
 void zerowarning( double test, double zero,double mult ) {
-/*
-    Prints a warning message if the value is
-    within the precision cutoff for zero
-*/
 	if( fabs( test ) <= zero*mult ) printf( "Warning: Value %e is within %lf times magnitude of zero check value of %e.\n",test,mult,zero );
 }
