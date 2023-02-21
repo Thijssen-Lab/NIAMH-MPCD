d7a0f80b563f3e34dcf0ca8eda259212cd413980
T Kozhukhov <t.kozhukhov1@gmail.com>
2023-02-21 12:23:07 +0000

    Squash. Implement velocity field dump

--- mpcd/headers/init.h
+++ mpcd/headers/init.h
@@ -54,6 +54,7 @@ void openhistDens( FILE **f,char dir[],char fname[],char ext[] );
 void openhistS( FILE **f,char dir[],char fname[],char ext[] );
 void openavenstrophy( FILE **f,char dir[],char fname[],char ext[] );
 void openflow( FILE **f,char dir[],char fname[],char ext[] );
+void openvel( FILE **f,char dir[],char fname[],char ext[] );
 void opentraj( int bc,FILE *fsolids[],char dir[],char filesolids[],char filesuffix[],char fileextension[] );
 void openplace( int i,FILE *fsolids[],char dir[],char filesolids[],char filesuffix[],char fileextension[] );
 void openenergy( FILE **f,char dir[],char fname[],char ext[] );
