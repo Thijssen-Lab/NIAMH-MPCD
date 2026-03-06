f318f8fabc77a78af4ec92c297a9a4b55a7a678b
alschodt <169532717+AlexanderSchodt@users.noreply.github.com>
2026-03-06 13:38:41 +0100

    Moved optical trap before streaming.

--- mpcd/subroutines/mpc.c
+++ mpcd/subroutines/mpc.c
@@ -686,7 +686,7 @@ void acc_Opt_Trap_BC( bc *WALL,double dt, int runtime, double t_on, double t_off
 
 	// calculate the force on the colloid
 	for (i = 0; i < DIM; ++i) {
-		dQ[i] = WALL->Q_old[i] - WALL->QOPT[i];  // distance between colloid and trap
+		dQ[i] = WALL->Q[i] - WALL->QOPT[i];  // distance between colloid and trap
 		OPT_force[i] = -KOPT * dQ[i] / mass;  // Hooke's law for the trap
 
 		WALL->dV[i] += dt*OPT_force[i];
@@ -4940,6 +4940,14 @@ void timestep(cell ***CL, particleMPC *SRDparticles, spec SP[], bc WALL[], simpt
 	#endif
 	//Apply impulse from MPC_BCcollision()
 
+	// apply optical trap forces if enabled
+	for (i = 0; i < NBC; ++i) {
+		if ((WALL+i)->DSPLC && (WALL+i)->ENABLEOPT) {
+			// Apply optical trap forces
+			acc_Opt_Trap_BC((WALL+i), in.dt, runtime, (WALL+i)->tOnOpt, (WALL+i)->tOffOpt, (WALL+i)->KOPT, (WALL+i)->VOPT, (WALL+i)->MASS);
+		}
+	}
+
 	if(!in.warmupSteps){
 		for( i=0; i<NBC; i++ ) if( (WALL+i)->DSPLC ) {
 			for( j=0; j<DIM; j++ ) (WALL+i)->V[j] += (WALL+i)->dV[j];
@@ -5025,13 +5033,6 @@ void timestep(cell ***CL, particleMPC *SRDparticles, spec SP[], bc WALL[], simpt
 	#endif
 	//Apply impulse from BC_MPCcollision()
 
-	// apply optical trap forces if enabled
-	for (i = 0; i < NBC; ++i) {
-		if ((WALL+i)->DSPLC && (WALL+i)->ENABLEOPT) {
-			// Apply optical trap forces
-			acc_Opt_Trap_BC((WALL+i), in.dt, runtime, (WALL+i)->tOnOpt, (WALL+i)->tOffOpt, (WALL+i)->KOPT, (WALL+i)->VOPT, (WALL+i)->MASS);
-		}
-	}
 
 	for( i=0; i<NBC; i++ ) if( (WALL+i)->DSPLC ) {
 		for( j=0; j<DIM; j++ ) (WALL+i)->V[j] += (WALL+i)->dV[j];
